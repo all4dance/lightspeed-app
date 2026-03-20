f465aab60e1d7503f56c1b67a822c72e7fc3b903
all4dance <damian@all4dance.ca>
2026-03-20 17:05:58 -0600

    Update data.js
    commit

--- src/routes/data.js
+++ src/routes/data.js
@@ -1,26 +1,31 @@
 const express = require('express')
 const router = express.Router()
-
 const { apiRequest } = require('../lightspeed')
 
 router.get('/items/:accountId', async (req, res) => {
-
   const { accountId } = req.params
 
   try {
-
     const data = await apiRequest(accountId, 'Item.json')
-
     res.json(data)
-
   } catch (err) {
-
     res.status(500).json({
       error: err.message
     })
-
   }
+})
 
+router.get('/sales/:accountId', async (req, res) => {
+  const { accountId } = req.params
+
+  try {
+    const data = await apiRequest(accountId, 'Sale.json')
+    res.json(data)
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    })
+  }
 })
 
 module.exports = router
