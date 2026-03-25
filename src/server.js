429ad5448046386d1c4d6d36a6c584622c2a7e85
all4dance <damian@all4dance.ca>
2026-03-25 13:18:21 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,6 +6,18 @@ const authRoutes = require('./routes/auth')
 const dataRoutes = require('./routes/data')
 
 const app = express()
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://reports.all4dance.ca')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204)
+  }
+
+  next()
+})
 app.use(cors({
   origin: [
     'https://reports.all4dance.ca',
