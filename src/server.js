4cd7ba03657892a58ed94b1519a6f38125cfd339
all4dance <damian@all4dance.ca>
2026-03-25 12:16:01 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const cors = require('cors')
 const authRoutes = require('./routes/auth')
 const dataRoutes = require('./routes/data')
 
 const app = express()
+app.use(cors({
+  origin: [
+    'https://reports.all4dance.ca',
+    'http://reports.all4dance.ca',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000'
+  ],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.use(express.json())
 
